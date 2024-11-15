ba9dacc859736751d141e2effd3c9bd6243216f4
Megan Ellman <meganlellman@gmail.com>
2024-11-15 14:41:59 -0800

    cleaned up code comments

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-// establish the player and computer
-// if there's a winning combo, give player a point, ask if they want to play again,
-// if there's a losing combo, give computer a point, ask if they want to play again
-
-
 let player = 0;
 let computer = 0;
-
 // objects for the game, winning combo and losing combo
 const winGame = {
     rock: 'scissors',
     scissors: 'paper',
     paper: 'rock'
 }
-
-// function to randomly pick for the computer
+// randomly selects weapon from array for computer's pick
 function randomPick(){
     let weapon = ['rock', 'paper', 'scissors'];
     return weapon[Math.floor(Math.random() * weapon.length)]
 }
-// function playGame that will call the random pick function to get the computer's selection, asks the user for a selection, and then scans the game object to see if there's a match for a win,
+// looks for player/computer pick in winGame obj, if found, then player wins
+// if same pick, tied
+// if not, player loses  
 function playGame(){
     let computerPick = randomPick();
+    // assigns prompt answer to playerPick variable
     let playerPick = (prompt('Rock, Paper, or Scissors').toLowerCase());
 
     console.log(`player: ${playerPick}, computer: ${computerPick}`)
@@ -37,7 +33,8 @@ function playGame(){
     }
     playAgain()
 }
-
+// if player confirms play again, then the playGame function will run again
+// if not, then the scores are reset 
 function playAgain(){
     let answer = confirm('Play again?');
     if(answer === true){
